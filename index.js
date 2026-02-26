944f97a43c00bb411c8fd0ebd268ca7ce10f8d42
Adil Haris Hamka <harishamka2009@gmail.com>
2026-02-26 23:02:06 +0700

    fix start script

--- index.js
+++ index.js
@@ -30,6 +30,8 @@ app.post("/hitung-umur", (req, res) => {
     res.json({ age });
 });
 
-app.listen(3000, () => {
-    console.log("Server jalan di http://localhost:3000 🚀");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server jalan di port ${PORT}`);
 });
