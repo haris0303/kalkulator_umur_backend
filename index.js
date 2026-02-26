b1979272a8bbd1e05e6cca6bb2c84b96da667515
Adil Haris Hamka <harishamka2009@gmail.com>
2026-02-26 22:17:56 +0700

    backend ready

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/hitung-umur", (req, res) => {
+    const { birthday } = req.body;
+
+    if (!birthday) {
+        return res.status(400).json({ message: "Tanggal lahir wajib diisi" });
+    }
+
+    const currentDate = new Date();
+    const birthdayDate = new Date(birthday);
+
+    let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+
+    const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+    if (
+        month < 0 ||
+        (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+    ) {
+        age--;
+    }
+
+    res.json({ age });
+});
+
+app.listen(3000, () => {
+    console.log("Server jalan di http://localhost:3000 🚀");
+});
